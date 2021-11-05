ee9bf8098470756e46000a5f89c104cb3d74da64
Vim Rivera <vrivera@you-source.com>
2021-11-06 03:16:12 +0800

    separated stations and node displays, initial interface draft, added features and functions 11/6/2021

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -9,16 +9,15 @@ import { NumberValueAccessor } from '@angular/forms';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  returnedVal: any;
   data: any;
   dataString: string;
-  komiLong: number;
-  komiLat: number;
-  toshioLong: number;
-  toshioLat: number;
-  komiDist: number;
-  toshioDist: number;
-  komiPerc: number;
-  toshioPerc: number;
+  //station1 variables
+  station1Long: number; station1Lat: number; station1Dist: number; station1Perc: number; station1transit: boolean = true;
+  Percentage1: number; loadingDisp1: boolean = true;
+  //station2 variables
+  station2Long: number; station2Lat: number; station2Dist: number; station2Perc: number; station2transit: boolean = true;
+  Percentage2: number; loadingDisp2: boolean = true;
 
   constructor(
     private dataService: ThingspeakDataService
@@ -33,25 +32,67 @@ export class Tab1Page {
   }
 
   async getData(): Promise<void> {
-    this.data = await this.dataService.getData();
+    this.returnedVal = await this.dataService.getData();
+    this.data = this.returnedVal.feeds[0].field1;
 
-    if (JSON.stringify(this.data).slice(286, 287) === "0") {
-      this.komiLat = parseFloat(JSON.stringify(this.data).slice(287, 296));
-      this.komiLong = parseFloat(JSON.stringify(this.data).slice(296, 306));
-      console.log("komi is at " + this.komiLat + " , " + this.komiLong);
-      this.komiDist = this.distanceFunc(this.komiLat, this.komiLong, 14, 121); //change start/dest coords
-      this.komiPerc = 1-(this.komiDist/100);
-      console.log("komi distance from dest is "+this.komiDist)
-      console.log("komi percentage is "+this.komiPerc);
+    //Station1
+    if (this.data.slice(0, 1) === "0") {
+      this.station1Lat = parseFloat(this.data.slice(1, 10));
+      this.station1Long = parseFloat(this.data.slice(10, 20));
+      console.log("station1 is at " + this.station1Lat + " , " + this.station1Long);
+
+      if (this.station1Lat != 0 && this.station1Long != 0) {
+        this.station1Dist = this.distanceFunc(this.station1Lat, this.station1Long, 14.564350, 121.097857); //station coordinates
+        this.station1Perc = 1 - (this.station1Dist / 0.10778681124005579); //denominator is distance from other station
+        this.Percentage1 = Math.round(this.station1Perc * 100);
+        //prevents undershoot, no need for overshoot because of allowance
+        if (this.Percentage1 <= 0) {
+          this.Percentage1 = 0;
+        }
+        //shows awaiting data if null
+        if (this.Percentage1 != null) {
+          this.loadingDisp1 = false;
+        }
+        //controls status display
+        if (this.station1Dist <= 0.006) { //display arrival when distance to station is less than 6m
+          this.station1transit = false; //not in transit
+          this.station1Perc = 1;
+        }
+        else {
+          this.station1transit = true; //in transit
+        }
+        console.log("station1 distance from dest is " + this.station1Dist)
+      }
     }
-    else if (JSON.stringify(this.data).slice(286, 287) === "1") {
-      this.toshioLat = parseFloat(JSON.stringify(this.data).slice(287, 296));
-      this.toshioLong = parseFloat(JSON.stringify(this.data).slice(296, 306));
-      console.log("toshio is at " + this.toshioLat + " , " + this.toshioLong);
-      this.toshioDist = this.distanceFunc(this.toshioLat, this.toshioLong, 14, 121); //change start/dest coords
-      this.toshioPerc = 1-(this.toshioDist/100);
-      console.log("toshio distance from dest is "+this.toshioDist);
-      console.log("toshio percentage is "+this.toshioPerc);
+
+    //Station2
+    else if (this.data.slice(0, 1) === "1") {
+      this.station2Lat = parseFloat(this.data.slice(1, 10));
+      this.station2Long = parseFloat(this.data.slice(10, 20));
+      console.log("station2 is at " + this.station2Lat + " , " + this.station2Long);
+
+      if (this.station2Lat != 0 && this.station1Long != 0) {
+        this.station2Dist = this.distanceFunc(this.station2Lat, this.station2Long, 14.564636, 121.096900); //station coordinates
+        this.station2Perc = 1 - (this.station2Dist / 0.10778681124005579); //denominator is distance from other station
+        this.Percentage2 = Math.round(this.station2Perc * 100);
+        //prevents undershoot
+        if (this.Percentage2 <= 0) {
+          this.Percentage2 = 0;
+        }
+        //shows awaiting data if null
+        if (this.Percentage2 != null) {
+          this.loadingDisp2 = false;
+        }
+        //controls status display
+        if (this.station2Dist <= 0.006) {
+          this.station2transit = false;
+          this.station2Perc = 1;
+        }
+        else {
+          this.station2transit = true;
+        }
+        console.log("station2 distance from dest is " + this.station2Dist);
+      }
     }
 
   }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ThingspeakDataService } from '../_shared/services';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +8,85 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  returnedVal: any;
+  data: any;
+  dataString: string;
+  komiLong: number;
+  komiLat: number;
+  toshioLong: number;
+  toshioLat: number;
+  komiDist: number;
+  toshioDist: number;
+  komiPerc: number;
+  toshioPerc: number;
 
-  constructor() {}
+  constructor(
+    private dataService: ThingspeakDataService
+  ) { }
+
+  ngOnInit(): void {
+    const obs$ = interval(1000);
+    obs$.subscribe((d) => {
+      this.getData();
+    }
+    )
+  }
+
+  async getData(): Promise<void> {
+    this.returnedVal = await this.dataService.getData();
+    this.data = this.returnedVal.feeds[0].field1;
+    console.log(this.data);
+
+    if (this.data.slice(0,1) === "0") {
+      this.komiLat = parseFloat(this.data.slice(1, 10));
+      this.komiLong = parseFloat(this.data.slice(10, 20));
+      console.log("komi is at " + this.komiLat + " , " + this.komiLong);
+      if (this.komiLat != 0 && this.komiLong != 0) {
+        this.komiDist = this.distanceFunc(this.komiLat, this.komiLong, 14, 121); //change start/dest coords
+        this.komiPerc = 1 - (this.komiDist / 100);
+        console.log("komi distance from dest is " + this.komiDist)
+        console.log("komi percentage is " + this.komiPerc);
+      }
+    }
+
+    else if (this.data.slice(0, 1) === "1") {
+      this.toshioLat = parseFloat(this.data.slice(1, 10));
+      this.toshioLong = parseFloat(this.data.slice(10, 20));
+      console.log("toshio is at " + this.toshioLat + " , " + this.toshioLong);
+      if (this.toshioLat != 0 && this.komiLong != 0) {
+        this.toshioDist = this.distanceFunc(this.toshioLat, this.toshioLong, 14, 121); //change start/dest coords
+        this.toshioPerc = 1 - (this.toshioDist / 100);
+        console.log("toshio distance from dest is " + this.toshioDist);
+        console.log("toshio percentage is " + this.toshioPerc);
+      }
+    }
+
+  }
+
+  //------------------HAVERSINE FORMULA (COORDINATES TO KM)------------------------//
+  //source: https://www.geodatasource.com/developers/javascript
+
+  distanceFunc(lat1, lon1, lat2, lon2) {
+    if ((lat1 == lat2) && (lon1 == lon2)) {
+      return 0;
+    }
+    else {
+      var radlat1 = Math.PI * lat1 / 180;
+      var radlat2 = Math.PI * lat2 / 180;
+      var theta = lon1 - lon2;
+      var radtheta = Math.PI * theta / 180;
+      var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+      if (dist > 1) {
+        dist = 1;
+      }
+      dist = Math.acos(dist);
+      dist = dist * 180 / Math.PI;
+      dist = dist * 60 * 1.1515;
+      dist = dist * 1.609344
+      return dist;
+    }
+  }
+
+
 
 }
