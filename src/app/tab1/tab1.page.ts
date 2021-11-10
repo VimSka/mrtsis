cf08367a60e979f8528bfc7f0da5ecc24aed15bc
Vim Rivera <vrivera@you-source.com>
2021-11-10 22:17:57 +0800

    added fix for same destination problem

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -13,6 +13,10 @@ export class Tab1Page {
   data: any;
   dataString: string;
   tachometerBit: number;
+  komiDist1: number = 200;
+  toshioDist1: number = 200;
+  komiDist2: number = 200;
+  toshioDist2: number = 200;
   //station1 variables
   station1Long: number; station1Lat: number; station1Dist: number; station1Perc: number; station1transit: boolean = true;
   Percentage1: number; loadingDisp1: boolean = true;
@@ -35,7 +39,11 @@ export class Tab1Page {
   async getData(): Promise<void> {
     this.returnedVal = await this.dataService.getData();
     this.data = this.returnedVal.feeds[0].field1;
-    this.tachometerBit = 0; //simulates moving train TACHOMETER OUTPUT
+    this.tachometerBit = 1; //simulates moving train TACHOMETER OUTPUT
+    console.log(this.komiDist1);
+    console.log(this.komiDist2);
+    console.log(this.toshioDist1);
+    console.log(this.toshioDist2);
 
     //trigger function if coordinates are valid
     if (this.data.slice(1, 10) != "00.000000" && this.tachometerBit == 1) {
@@ -46,26 +54,9 @@ export class Tab1Page {
         console.log("station1 is at " + this.station1Lat + " , " + this.station1Long);
 
         if (this.station1Lat != 0 && this.station1Long != 0) {
-          this.station1Dist = this.distanceFunc(this.station1Lat, this.station1Long, 14.564350, 121.097857); //station coordinates
-          this.station1Perc = 1 - (this.station1Dist / 0.10778681124005579); //denominator is distance from other station
-          this.Percentage1 = Math.round(this.station1Perc * 100);
-          //prevents undershoot, no need for overshoot because of allowance
-          if (this.Percentage1 <= 0) {
-            this.Percentage1 = 0;
-          }
-          //shows awaiting data if null
-          if (this.Percentage1 != null) {
-            this.loadingDisp1 = false;
-          }
-          //controls status display
-          if (this.station1Dist <= 0.006) { //display arrival when distance to station is less than 6m
-            this.station1transit = false; //not in transit
-            this.station1Perc = 1;
-          }
-          else {
-            this.station1transit = true; //in transit
-          }
-          console.log("station1 distance from dest is " + this.station1Dist)
+          this.station1CompareDist();
+          this.station1Function();
+          console.log("distance from station1 is " + this.station1Dist)
         }
       }
 
@@ -76,32 +67,14 @@ export class Tab1Page {
         console.log("station2 is at " + this.station2Lat + " , " + this.station2Long);
 
         if (this.station2Lat != 0 && this.station1Long != 0) {
-          this.station2Dist = this.distanceFunc(this.station2Lat, this.station2Long, 14.564636, 121.096900); //station coordinates
-          this.station2Perc = 1 - (this.station2Dist / 0.10778681124005579); //denominator is distance from other station
-          this.Percentage2 = Math.round(this.station2Perc * 100);
-          //prevents undershoot
-          if (this.Percentage2 <= 0) {
-            this.Percentage2 = 0;
-          }
-          //shows awaiting data if null
-          if (this.Percentage2 != null) {
-            this.loadingDisp2 = false;
-          }
-          //controls status display
-          if (this.station2Dist <= 0.006) {
-            this.station2transit = false;
-            this.station2Perc = 1;
-          }
-          else {
-            this.station2transit = true;
-          }
-          console.log("station2 distance from dest is " + this.station2Dist);
-          console.log("station1 distance from dest " + this.station1Dist);
+          this.station2CompareDist();
+          this.station2Function();
+          console.log("distance from station2 is " + this.station2Dist);
         }
       }
     }
-  
-//----------------UNDERGROUND ALGORITHM--------------------------
+
+    //----------------UNDERGROUND ALGORITHM--------------------------
     else if (this.data.slice(1, 10) == "00.000000" && this.tachometerBit == 1) {
       console.log("underground mode");
       console.log(this.station1Dist == undefined);
@@ -109,48 +82,14 @@ export class Tab1Page {
       if (this.data.slice(0, 1) == 0 && this.station1Dist != undefined) {
         console.log("station 1 lost during transit");
         this.station1Dist = this.station1Dist - (5 / 3600); //5km/h in km/s
-        this.station1Perc = 1 - (this.station1Dist / 0.10778681124005579); //denominator is distance from other station
-        this.Percentage1 = Math.round(this.station1Perc * 100);
-        //prevents undershoot, no need for overshoot because of allowance
-        if (this.Percentage1 <= 0) {
-          this.Percentage1 = 0;
-        }
-        //shows awaiting data if null
-        if (this.Percentage1 != null) {
-          this.loadingDisp1 = false;
-        }
-        //controls status display
-        if (this.station1Dist <= 0.006) { //display arrival when distance to station is less than 6m
-          this.station1transit = false; //not in transit
-          this.station1Perc = 1;
-        }
-        else {
-          this.station1transit = true; //in transit
-        }
+        this.station1Function();
         console.log("station1 distance from dest is " + this.station1Dist)
       }
       //STATION 2 lost in between
       else if (this.data.slice(0, 1) == 1 && this.station1Dist != undefined) {
         console.log("station 2 lost during transit");
         this.station2Dist = this.station2Dist - (5 / 3600);
-        this.station2Perc = 1 - (this.station2Dist / 0.10778681124005579); //denominator is distance from other station
-        this.Percentage2 = Math.round(this.station2Perc * 100);
-        //prevents undershoot
-        if (this.Percentage2 <= 0) {
-          this.Percentage2 = 0;
-        }
-        //shows awaiting data if null
-        if (this.Percentage2 != null) {
-          this.loadingDisp2 = false;
-        }
-        //controls status display
-        if (this.station2Dist <= 0.006) {
-          this.station2transit = false;
-          this.station2Perc = 1;
-        }
-        else {
-          this.station2transit = true;
-        }
+        this.station2Function();
         console.log("station2 distance from dest is " + this.station2Dist);
       }
       //STATION 1 lost from the start
@@ -158,24 +97,7 @@ export class Tab1Page {
         console.log("station 1 lost from start");
         this.station1Dist = 0.107;
         this.station1Dist = this.station1Dist - (5 / 3600); //5km/h in km/s
-        this.station1Perc = 1 - (this.station1Dist / 0.10778681124005579); //denominator is distance from other station
-        this.Percentage1 = Math.round(this.station1Perc * 100);
-        //prevents undershoot, no need for overshoot because of allowance
-        if (this.Percentage1 <= 0) {
-          this.Percentage1 = 0;
-        }
-        //shows awaiting data if null
-        if (this.Percentage1 != null) {
-          this.loadingDisp1 = false;
-        }
-        //controls status display
-        if (this.station1Dist <= 0.006) { //display arrival when distance to station is less than 6m
-          this.station1transit = false; //not in transit
-          this.station1Perc = 1;
-        }
-        else {
-          this.station1transit = true; //in transit
-        }
+        this.station1Function();
         console.log("station1 distance from dest is " + this.station1Dist)
       }
       //STATION 2 lost from the start
@@ -183,24 +105,7 @@ export class Tab1Page {
         console.log("station 2 lost from start");
         this.station2Dist = 0.107;
         this.station2Dist = this.station2Dist - (5 / 3600);
-        this.station2Perc = 1 - (this.station2Dist / 0.10778681124005579); //denominator is distance from other station
-        this.Percentage2 = Math.round(this.station2Perc * 100);
-        //prevents undershoot
-        if (this.Percentage2 <= 0) {
-          this.Percentage2 = 0;
-        }
-        //shows awaiting data if null
-        if (this.Percentage2 != null) {
-          this.loadingDisp2 = false;
-        }
-        //controls status display
-        if (this.station2Dist <= 0.006) {
-          this.station2transit = false;
-          this.station2Perc = 1;
-        }
-        else {
-          this.station2transit = true;
-        }
+        this.station2Function();
         console.log("station2 distance from dest is " + this.station2Dist);
       }
     }
@@ -231,4 +136,83 @@ export class Tab1Page {
     }
   }
 
+  //-------------Common functions--------------------//
+  station1Function() {
+    this.station1Perc = 1 - (this.station1Dist / 0.10778681124005579); //denominator is distance from other station
+    this.Percentage1 = Math.round(this.station1Perc * 100);
+    //prevents undershoot, no need for overshoot because of allowance
+    if (this.Percentage1 <= 0) {
+      this.Percentage1 = 0;
+    }
+    //shows awaiting data if null
+    if (this.Percentage1 != null) {
+      this.loadingDisp1 = false; //not loading
+    }
+    //controls status display
+    if (this.station1Dist <= 0.012) { //display arrival when distance to station is less than 6m
+      this.station1transit = false; //not in transit
+      this.station1Perc = 1;
+    }
+    else {
+      this.station1transit = true; //in transit
+    }
+  }
+
+  station2Function() {
+    this.station2Perc = 1 - (this.station2Dist / 0.10778681124005579); //denominator is distance from other station
+    this.Percentage2 = Math.round(this.station2Perc * 100);
+    //prevents undershoot
+    if (this.Percentage2 <= 0) {
+      this.Percentage2 = 0;
+    }
+    //shows awaiting data if null
+    if (this.Percentage2 != null) {
+      this.loadingDisp2 = false;
+    }
+    //controls status display
+    if (this.station2Dist <= 0.012) {
+      this.station2transit = false;
+      this.station2Perc = 1;
+    }
+    else {
+      this.station2transit = true;
+    }
+  }
+
+  //-------------Nearest Node Algorithm--------------//
+  station1CompareDist() {
+    if (this.data.slice(20, 21) == "K") {
+      this.komiDist2 = 200;
+      this.komiDist1 = this.distanceFunc(this.station1Lat, this.station1Long, 14.564350, 121.097857);
+    }
+    else if(this.data.slice(20,21) == "T"){
+      this.toshioDist2 = 200;
+      this.toshioDist1 = this.distanceFunc(this.station1Lat, this.station1Long, 14.564350, 121.097857);
+    }
+    //compares distances and sets nearest node for progress bar
+    if (this.komiDist1 <= this.toshioDist1) {
+      this.station1Dist = this.komiDist1;
+    }
+    else {
+      this.station1Dist = this.toshioDist1;
+    }
+  }
+
+  station2CompareDist(){
+    if (this.data.slice(20, 21) == "K") {
+      this.komiDist1 = 200;
+      this.komiDist2 = this.distanceFunc(this.station2Lat, this.station2Long, 14.564636, 121.096900);
+    }
+    else if(this.data.slice(20,21) == "T"){
+      this.toshioDist1 = 200;
+      this.toshioDist2 = this.distanceFunc(this.station2Lat, this.station2Long, 14.564636, 121.096900);
+    }
+    //compares distances and sets nearest node for progress bar
+    if (this.komiDist2 <= this.toshioDist2) {
+      this.station2Dist = this.komiDist2;
+    }
+    else {
+      this.station2Dist = this.toshioDist2;
+    }
+  }
 }
